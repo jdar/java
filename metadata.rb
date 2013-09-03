be628c1f9849519e691f5fb03828e1f54a125bd4
Darius Roberts <darius.roberts@gmail.com>
2013-09-03 13:00:06 -0400

    fool me four times, shame on me... need to update master cookbooks repo (or learn to upload via s3 zip)

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,10 @@ description       "Installs Java runtime."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.10.0"
 
-recipe "java" #, "Installs Java runtime"
-recipe "java::openjdk" #, "Installs the OpenJDK flavor of Java"
-#provides "java::oracle"#, "Installs the Oracle flavor of Java"
-#provides "java::oracle_i386"#, "Installs the 32-bit jvm without setting it as the default"
+provides "java" #, "Installs Java runtime"
+provides "java::openjdk" #, "Installs the OpenJDK flavor of Java"
+provides "java::oracle"#, "Installs the Oracle flavor of Java"
+provides "java::oracle_i386"#, "Installs the 32-bit jvm without setting it as the default"
 
 %w{ debian ubuntu centos redhat scientific fedora amazon arch oracle freebsd windows }.each do |os|
   supports os
